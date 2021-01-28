fbf7b4fba8e12e77cba9471b05d55d2420b6dbcb
youxia123-cloud <311375@qq.com>
2021-01-28 22:49:40 +0800

    寒假考核3

--- javascript/index1.js
+++ javascript/index1.js
@@ -1,23 +1,38 @@
-var form = document.querySelector('#form');
-var yhm = document.querySelector('#yhm');
-var mm = document.querySelector('#mm');
-var btn = document.querySelector('#tj');
-btn.onclick = function(){
+var btn=document.querySelector('#submit');
+btn.onclick=function(){
 
-//实例化XMLHttpRequest对象
-const xhr=new XMLHttpRequest();
-xhr.open('get','http://musicapi.leanapp.cn?phone=+"yhm.value"+&password=+"mm.value"',true);//初始化一个get请求
-xhr.onreadystatechange = ()=> {
-    if(xhr.readyState ===4){
-        if((xhr.status >= 200 &&xhr.status < 300)||xhr.status ==304){
-            const res=JSON.parse(xhr.responseText);
-            console.log("请求成功");
-           var data=res;
-           console.log(data);
-        }else{
-            console.log("请求失败");
-        }
-    }
-};
-//发送请求
-xhr.send();}
+var xmlhttp;
+if (window.XMLHttpRequest){//IE7+, Firefox, Chrome, Opera, Safari
+       xmlhttp=new XMLHttpRequest();
+}
+else{// IE6, IE5
+       xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+//上面的http请求对象的生成做了一个浏览器兼容性处理
+var adminName = document.getElementById('adminName').value;//获取html表单中adminName输入域对象的值，既账号
+var psw = document.getElementById('psw').value;//获取html表单中pwd输入域对象的值，既密码          
+ 
+xmlhttp.onreadystatechange=function(){
+//当接受到响应时回调该方法
+        if (xmlhttp.readyState==4 && (xmlhttp.status==200||xmlhttp.status==0))
+        {
+                    var tip = document.getElementById('tip');//获取html的tip节点，主要用于输出登录结果
+                    var text = xmlhttp.responseText;//使用接口返回内容，响应内容
+                    var resultJson = eval("("+text+")");//把响应内容对象转成javascript对象
+                    var result = resultJson.result;//获取json中的result键对应的值
+                    var code = resultJson.code;//获取json中的code键对应的值
+                    if (result=="fail") {//登录失败
+                        if(code==101){
+                            tip.innerHTML = "密码错误!"
+                        }else if(code==102){
+                            tip.innerHTML = "用户不存在!"
+                        }
+                    }else //登录成功        if(result=="success"&&code==100){
+                        window.open("../html/index3.html");//跳转到index3.html页面
+                    }
+          }
+
+ xmlhttp.open("POST","http://musicapi.leanapp.cn/login/cellphone?phone="+adminName+"&password="+psw+new Date().getTime(),true);//以POST方式请求该接口
+ xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");//添加Content-type
+ xmlhttp.send("adminName="+adminName+"&psw="+psw);//发送请求参数间用&分割
+}
