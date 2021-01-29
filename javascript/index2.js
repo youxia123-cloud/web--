a091a63d3fa267c556b2e42d47830c6c61a2d100
youxia123-cloud <311375@qq.com>
2021-01-29 21:21:20 +0800

    寒假考核4

--- javascript/index2.js
+++ javascript/index2.js
@@ -1,19 +1,3 @@
-/*实现登录功能*/
-let btnLogin=document.querySelector('.creator_Center');
-btnLogin.onclick = function(){
-    window.open("../html/index1.html");
-}
-let btnLogin1=document.querySelector('.login');
-btnLogin1.onclick = function(){
-    window.open("../html/index2.html","登录",
-            "height=600px,width=700px,top=30px, left=430px, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no")
-}/*top=0 窗口距离屏幕上方的象素值
-left=0 窗口距离屏幕左侧的象素值
-toolbar=no 是否显示工具栏，yes为显示；
-menubar，scrollbars 表示菜单栏和滚动栏
-resizable=no 是否允许改变窗口大小，yes为允许；
-location=no 是否显示地址栏，yes为允许；
-status=no 是否显示状态栏内的信息（通常是文件已经打开），yes为允许*/
 //实例化XMLHttpRequest对象
 const xhr=new XMLHttpRequest();
 xhr.open('get','http://musicapi.leanapp.cn/top/playlist?limit=10&order=new',true);//初始化一个get请求
